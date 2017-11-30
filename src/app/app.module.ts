cb22bdace511e7b3461644fe65041288be2797d8
Galdin Raphael <gldraphael@gmail.com>
2017-11-30 18:00:17 +0530

    Generate the components for our editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { EditAreaComponent } from './edit-area/edit-area.component';
+import { PreviewAreaComponent } from './preview-area/preview-area.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    EditAreaComponent,
+    PreviewAreaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/edit-area/edit-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-area',
+  templateUrl: './edit-area.component.html',
+  styleUrls: ['./edit-area.component.css']
+})
+export class EditAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
