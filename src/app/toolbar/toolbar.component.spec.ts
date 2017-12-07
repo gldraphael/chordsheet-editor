90dccf1812265cfba2f85f01d0064141b940770d
Galdin Raphael <gldraphael@gmail.com>
2017-12-07 12:04:21 +0530

    Write tests for the toolbar

--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ToolbarComponent } from './toolbar.component'
+import { ToolbarCommandService } from './toolbar-command.service';
+import { By } from '@angular/platform-browser';
 
-describe('ToolbarComponent', () => {
+function getMobilePreviewElement(fixture: ComponentFixture<ToolbarComponent>) {
+  const preview = fixture.debugElement.query(By.css('ul li:first-of-type a span.small-screen'))
+  return preview.nativeElement as HTMLSpanElement
+}
+
+fdescribe('ToolbarComponent', () => {
   let component: ToolbarComponent
   let fixture: ComponentFixture<ToolbarComponent>
+  let toolbarCommandService: ToolbarCommandService
+
+  beforeAll(() => {
+    toolbarCommandService = new ToolbarCommandService()
+  })
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ ToolbarComponent ],
+      providers: [
+        { provide: ToolbarCommandService, useValue: toolbarCommandService }
+      ]
     })
     .compileComponents()
   }))
@@ -22,4 +37,16 @@ describe('ToolbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should show Edit on startup (because the preview tab is open)', () => {
+    expect(getMobilePreviewElement(fixture).textContent).toEqual('Edit')
+  })
+
+  it('should show Preview once the preview pane visibility is toggled', () => {
+    toolbarCommandService.togglePreviewPaneVisibility()
+    fixture.detectChanges()
+
+    expect(getMobilePreviewElement(fixture).textContent).toEqual('Preview')
+  })
+
 })
