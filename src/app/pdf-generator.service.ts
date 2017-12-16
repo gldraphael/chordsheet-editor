8dcd78e123d626cbf586ef250aa8d45a98be40c0
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-16 16:40:06 +0530

    Add PDF Support
    PR #3

---
+++ src/app/pdf-generator.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
+
+@Injectable()
+export class PdfGeneratorService {
+
+  constructor() { }
+
+  public toPdf(filename: string, element: HTMLElement) {
+    const options = {
+      margin: .75,
+      filename: filename.toLowerCase().endsWith('.pdf') ? filename : filename + '.pdf',
+      image: {
+        type: 'jpeg',
+        quality: 1
+      },
+      html2canvas: {
+        dpi: 300,
+        letterRendering: true
+      },
+      jsPDF: {
+        unit: 'in',
+        format: 'letter',
+        orientation: 'portrait'
+      }
+    }
+    html2pdf(element, options);
+  }
+
+}
